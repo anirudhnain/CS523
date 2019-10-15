e81d1c8cd20b34529a68fac5f058b86f26eb176d
Anirudh Nain <anain2@illinois.edu>
2019-10-14 23:10:50 -0500

    Adding read and write to s3

---
+++ cloudFsRead.py
@@ -0,0 +1,44 @@
+import os.path
+from os import path
+
+import boto3
+
+import time
+
+def readFromS3(s3, key):
+    bucket = "speedfs"
+    obj = s3.Object(bucket, key)
+    # print(obj.get()['Body'].read().decode('utf-8'))
+
+# fileName = "test.txt"
+# fileNumber = 1
+
+# newFile = open(fileName,'w+')
+
+start_time = time.time()
+
+KEY_ID = "AKIATJYDBBGBAKD5IBUD"
+ACCESS_KEY = "BNEWUZ9l282GlsAclIrMnTPSsQu4iHyJCK+BVy9W"
+
+s3 = boto3.resource(
+    's3',
+    aws_access_key_id=KEY_ID,
+    aws_secret_access_key=ACCESS_KEY
+)
+
+readFromS3(s3, "testing/test132.txt")
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
+# while True:
+#     extension = fileName.split(".")[-1]
+#     chunkName = fileName.split(".")[0] + str(file_number) + "."+extension
+#     if path.exists(chunkFileName):
+#         with open(chunkFileName) as f:
+#             chunk = f.read()
+#             with open(fileName, 'a') as merge_file:
+#                 merge_file.write(chunk)
+#             fileNumber+=1
+#         os.remove(chunkFileName)
+#     else:
+#         break
---
+++ cloudFsWrite.py
@@ -0,0 +1,62 @@
+import boto3
+import random
+import string
+import os.path
+import sys
+import time
+from os import path
+
+def upload_file(file_name, bucket, s3, content):
+    print(file_name)
+    PREFIX = 'testing/'
+    s3.Object(bucket, PREFIX+file_name).put(Body=content)
+
+# def createChunksForFileAndUploadToS3(fileName, bucket, s3, CHUNK_SIZE):
+    
+#     if not path.exists(fileName):
+#         print ("File does not exist")
+#         sys.exit()
+
+#     file_number = 1
+#     chunksNames = []
+
+#     with open(fileName) as f:
+#         chunk = f.read(CHUNK_SIZE)
+#         while chunk:
+#             extension = fileName.split(".")[-1]
+#             chunkName = fileName.split(".")[0] + str(file_number) + "."+extension
+#             with open(chunkName, 'w+') as chunk_file:
+#                 chunk_file.write(chunk)
+#             chunksNames.append(chunkName)
+#             upload_file(chunkName, bucket, s3, open(chunkName, "r").read())
+#             file_number += 1
+#             chunk = f.read(CHUNK_SIZE)
+#     return chunksNames
+
+
+KEY_ID = "AKIATJYDBBGBAKD5IBUD"
+ACCESS_KEY = "BNEWUZ9l282GlsAclIrMnTPSsQu4iHyJCK+BVy9W"
+bucket = "speedfs"
+
+s3 = boto3.resource(
+    's3',
+    aws_access_key_id=KEY_ID,
+    aws_secret_access_key=ACCESS_KEY
+)
+
+# #Enter the file name you want to read from the system
+# fileName = input("Enter fileName you want to write to S3: ")
+
+# chunksNames = createChunksForFileAndUploadToS3(fileName, bucket, s3, 8000000)
+
+start_time = time.time()
+
+# write to last chunk
+with open("test.txt", 'a') as chunk_file:
+    chunk_file.write("  *******append new data**********")
+
+# print (open("test131.txt", "r").read())
+upload_file("test.txt", bucket, s3, open("test.txt", "r").read())
+
+
+print("--- %s seconds ---" % (time.time() - start_time))
